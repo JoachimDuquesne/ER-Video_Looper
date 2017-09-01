52a04ff1d648650a6ecc18b710cc51b31f7b7220
JoachimDuquesne <joachim.duquesne@gmail.com>
2017-09-01 12:44:17 +0200

    Service name updated

--- configure.sh
+++ configure.sh
@@ -1,27 +1,27 @@
 #! /bin/bash
 
 
-PROGRAM_DIR=/home/EscapeRush/Entrance_Room
+PROGRAM_DIR=/home/EscapeRush/Fitting_Room
 
 
 cd /etc/systemd/system
 
 # Clear the file
-echo "" >  Escape_Rush_Entrance_Room_Video_Looper.service
+echo "" >  Fitting_Room_Video_Looper.service
 
 # Write the file
-echo "[Unit]"                                             >> Escape_Rush_Entrance_Room_Video_Looper.service
-echo "Description=Entrance-room video looper="            >> Escape_Rush_Entrance_Room_Video_Looper.service
-#echo "After=multi-user.target"                           >> Escape_Rush_Entrance_Room_Video_Looper.service
-echo ""                                                   >> Escape_Rush_Entrance_Room_Video_Looper.service
-echo "[Service]"                                          >> Escape_Rush_Entrance_Room_Video_Looper.service
-echo "Type=idle"                                          >> Escape_Rush_Entrance_Room_Video_Looper.service
-echo "ExecStart=$PROGRAM_DIR/Entrance-room-video-looper"  >> Escape_Rush_Entrance_Room_Video_Looper.service
-echo ""                                                   >> Escape_Rush_Entrance_Room_Video_Looper.service
-echo "[Install]"                                          >> Escape_Rush_Entrance_Room_Video_Looper.service
-echo "WantedBy=multi-user.target"                         >> Escape_Rush_Entrance_Room_Video_Looper.service
+echo "[Unit]"                                             >> Fitting_Room_Video_Looper.service
+echo "Description=Fitting-room video looper="             >> Fitting_Room_Video_Looper.service
+#echo "After=multi-user.target"                           >> Fitting_Room_Video_Looper.service
+echo ""                                                   >> Fitting_Room_Video_Looper.service
+echo "[Service]"                                          >> Fitting_Room_Video_Looper.service
+echo "Type=idle"                                          >> Fitting_Room_Video_Looper.service
+echo "ExecStart=$PROGRAM_DIR/FittingRoom"                 >> Fitting_Room_Video_Looper.service
+echo ""                                                   >> Fitting_Room_Video_Looper.service
+echo "[Install]"                                          >> Fitting_Room_Video_Looper.service
+echo "WantedBy=multi-user.target"                         >> Fitting_Room_Video_Looper.service
 
 systemctl daemon-reload
-systemctl enable Escape_Rush_Entrance_Room_Video_Looper.service
+systemctl enable Fitting_Room_Video_Looper.service
 
 echo "Configuration completed"
